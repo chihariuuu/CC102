1d449cb6778e952d517ec2c8adc3e1afa0a28b3b
Princess Joy Masinas <masinaspj27@gmail.com>
2024-11-19 11:29:56 +0800

    Add files via upload

---
+++ Movie03_Optimized.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    double money;
+    bool parent;
+    string toPrintShow, toPrintMoney;
+	cout << "Enter age: "; 
+    cin >> age;
+
+    if (age < 13) {
+        cout << "With parent? (1 If Yes, 0 If No): ";
+        cin >> parent;
+        toPrintShow = parent ? "PG & G Show." : "G Show.";
+    } else if (age < 16) {
+    } else {
+        toPrintShow = "R, PG & G Show.";}
+	cout << "Enter money: ";
+    cin >> money;
+ toPrintMoney = money < 7.5 ? "Not enough money." : "Can go to evening & matinee show.";
+ cout << toPrintShow << endl;
+    cout << toPrintMoney << endl;
+ return 0;
+}
