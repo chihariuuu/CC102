ba94380b71a218ef594d1d74f50d472ae50febbf
Princess Joy Masinas <masinaspj27@gmail.com>
2024-11-30 21:29:00 +0800

    Add files via upload

---
+++ Activity01C.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int rows = 8;
+    int i = 0;
+
+    // outer loop
+    while (i < rows) {
+
+        // inner loop for spaces
+        int j = 0;
+        while (j < i) {
+            cout << "  ";
+            j++;
+        }
+
+        // inner loop for printing hashes
+        int h = 0;
+        while (h < rows - i) {
+            cout << "# ";
+            h++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
